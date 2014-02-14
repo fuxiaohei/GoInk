686effb55bb02fc696d5b922b4aab09f5a5c0d38
FuXiaoHei <fuxiaohei@hexiaz.com>
2014-02-14 23:57:00 +0800

    view cache support

--- view.go
+++ view.go
@@ -13,18 +13,41 @@ type View struct {
 	Dir string
 	// view functions map
 	FuncMap template.FuncMap
+	// Cache Flag
+	IsCache bool
+	// template cache map
+	templateCache map[string]*template.Template
+}
+
+func (v *View) getTemplateInstance(tpl string) (*template.Template, error) {
+	// if IsCache, get cached template if exist
+	if v.IsCache {
+		if v.templateCache[tpl] != nil {
+			return v.templateCache[tpl], nil
+		}
+	}
+	var (
+		t    *template.Template
+		e    error
+		file = path.Join(v.Dir, tpl)
+	)
+	t = template.New(path.Base(tpl))
+	t.Funcs(v.FuncMap)
+	t, e = t.ParseFiles(file)
+	if e != nil {
+		return nil, e
+	}
+	if v.IsCache {
+		v.templateCache[tpl] = t
+	}
+	return t, nil
+
 }
 
 // Render renders template with data.
 // Tpl is the filename under template directory.
 func (v *View) Render(tpl string, data map[string]interface{}) ([]byte, error) {
-	t := template.New(path.Base(tpl))
-	t.Funcs(v.FuncMap)
-	var (
-		e    error
-		file = path.Join(v.Dir, tpl)
-	)
-	t, e = t.ParseFiles(file)
+	t, e := v.getTemplateInstance(tpl)
 	if e != nil {
 		return nil, e
 	}
@@ -52,5 +75,7 @@ func NewView(dir string) *View {
 	v.FuncMap["Html"] = func(str string) template.HTML {
 		return template.HTML(str)
 	}
+	v.IsCache = false
+	v.templateCache = make(map[string]*template.Template)
 	return v
 }
